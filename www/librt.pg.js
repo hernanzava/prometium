88a1151d7c74f8e02b7966cfa62da6329bd57c3e
hernanzava <hernan.zava@enerminds.com>
2015-09-02 12:48:36 -0300

    cambio url

--- www/librt.pg.js
+++ www/librt.pg.js
@@ -241,7 +241,7 @@ function evalUpdated(name,cbok,cbfail) {
 
 //S: init
 //CFG_APPURL_DFLT= 'https://rtmovil.enerminds.com:8443/app/js';
-CFG_APPURL_DFLT= 'https://192.168.1.122:8443/app/Map';
+CFG_APPURL_DFLT= 'https://10.70.251.69:8443/app/Map';
 CFGLIB.appUrl= CFG_APPURL_DFLT;
 SRC_KEY= "18273hjsjacjhq83qq3dhsjdhdy38znddj"; //XXX: ofuscar
 function runApp() { //XXX:generalizar usando evalUpdated
