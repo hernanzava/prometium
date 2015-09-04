601fc3f567739d0db0795efba58d75658515add8
hernanzava <hernan.zava@enerminds.com>
2015-09-04 14:53:51 -0300

    adsada

--- www/librt.pg.js
+++ www/librt.pg.js
@@ -264,7 +264,7 @@ function evalUpdated(name,cbok,cbfail) {
 
 //S: init
 //CFG_APPURL_DFLT= 'https://rtmovil.enerminds.com:8443/app/js';
-CFG_APPURL_DFLT= 'https://10.70.251.69:8443/app/js';
+CFG_APPURL_DFLT= 'https://10.70.251.80:8443/app/js';
 CFGLIB.appUrl= CFG_APPURL_DFLT;
 SRC_KEY= "18273hjsjacjhq83qq3dhsjdhdy38znddj"; //XXX: ofuscar
 function runApp() { //XXX:generalizar usando evalUpdated
